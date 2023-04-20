b4448c20b30d1f7a856e7eacdd9e252d68af32c8
Charlie Powell <cdp1337@veraciousnetwork.com>
2023-04-19 21:33:51 -0400

    Feature #2 - bulk of the work for more images
    More testing will be needed, but just trying to get things organized

--- app/services/update_status_service.rb
+++ app/services/update_status_service.rb
@@ -71,7 +71,7 @@ class UpdateStatusService < BaseService
 
     raise Mastodon::ValidationError, I18n.t('media_attachments.validations.too_many') if @options[:media_ids].size > 4 || @options[:poll].present?
 
-    media_attachments = @status.account.media_attachments.where(status_id: [nil, @status.id]).where(scheduled_status_id: nil).where(id: @options[:media_ids].take(4).map(&:to_i)).to_a
+    media_attachments = @status.account.media_attachments.where(status_id: [nil, @status.id]).where(scheduled_status_id: nil).where(id: @options[:media_ids].take(24).map(&:to_i)).to_a
 
     raise Mastodon::ValidationError, I18n.t('media_attachments.validations.images_and_video') if media_attachments.size > 1 && media_attachments.find(&:audio_or_video?)
     raise Mastodon::ValidationError, I18n.t('media_attachments.validations.not_ready') if media_attachments.any?(&:not_processed?)
